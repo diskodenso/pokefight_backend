06afd24a8524ab50aa9a49bde3febe261d8e4f7a
Denso666 <Denisolf@mx.de>
2022-05-31 17:44:07 +0200

    add controller, routes and Api Documentation

--- controllers/pokemonsController.js
+++ controllers/pokemonsController.js
@@ -1,1 +1,41 @@
-console.log("here should be the controllers")
+// import json data
+import { readFile } from "fs/promises";
+// declare json variable and parse
+const pokemons = JSON.parse(
+  await readFile(new URL("../pokedex.json", import.meta.url))
+);
+// every controller needs to be exported seperately as you only can export one
+//------- get all countries controller ---------//
+export const getAllPokemons = async (req, res) => {
+    try {
+      const allPokemons = await pokemons
+    if (pokemons) {
+      res.status(200).json(pokemons);
+    } else {
+      res.status(500).json(error);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+//------- get single country controller ---------//
+export const getSinglePokemon = async (req, res) => {
+  console.log(req.params);
+  try {
+    const findPokemon = await pokemons.find(
+      (pokemon) => pokemon.id == req.params.id
+    );
+    if (findPokemon) {
+      res.status(200).json(findPokemon);
+    } else {
+      res.status(404).send("There is no Pokemon matching this ID");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+//------- delete country controller ---------//
+//(Optional) Create a GET route on /pokemon/:id/:info (<name>|<type>|<base>) which gives only one pokemon from
+// the JSON thanks to its id and retrieve only one information
+//(name or type or base) to send back to the client
--- routes/pokemonsRouter.js
+++ routes/pokemonsRouter.js
@@ -1,9 +1,16 @@
 // import router
-import {Router} from 'express';
+import { Router } from "express";
+import {
+  getAllPokemons,
+  getSinglePokemon,
+} from "../controllers/pokemonsController.js";
+// import controllers
 
 // declare router
-const pokemonsRouter = Router(); 
-// declare routes
-
+const pokemonsRouter = Router();
+// declare routes for all pokemons
+pokemonsRouter.route("/").get(getAllPokemons);
+// declare routes for single pokemon
+pokemonsRouter.route("/:id").get(getSinglePokemon);
 // export router
 export default pokemonsRouter;
